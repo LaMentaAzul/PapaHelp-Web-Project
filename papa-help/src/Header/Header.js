2440524e78e12b2f179a566aee11c7a95cfdf3ee
Mina Tahaei <bluxixi@outlook.com>
2019-07-23 13:31:52 +0430

    add final header/logo style

--- papa-help/src/Header/Header.js
+++ papa-help/src/Header/Header.js
@@ -7,15 +7,15 @@ export default class Header extends Component {
       <div>
         <img src={pic} className='pic' />
         <ul className='menu-ul'>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/src/Header/Problem.js'>Problem</Link></li>
-          <li><Link to='/src/Header/Presse.js'>Presse</Link></li>
-          <li><Link to='/src/Header/Losungsansatze.js'>Losungsansatze</Link></li>
-          <li><Link to='/src/Header/Literatur.js'>Literatur</Link></li>
-          <li><Link to='/src/Header/Gewalt.js'>Gewalt</Link></li>
-          <li><Link to='/src/Header/Forderungen.js'>Forderungen</Link></li>
-          <li><Link to='/src/Header/HilfePapa.js'>HilfePapa</Link></li>
-          <li><Link to='/src/Header/ENGLISH.js'>ENGLISH</Link></li>
+          <li><Link to='/' style={{ textDecoration: 'none', color: '#5B5454',  fontSize: '20px' }}>Home</Link></li>
+          <li><Link to='/src/Header/Problem.js' style={{ textDecoration: 'none', color: '#5B5454', fontSize: '20px' }} >Problem</Link></li>
+          <li><Link to='/src/Header/Presse.js' style={{ textDecoration: 'none', color: '#5B5454',  fontSize: '20px' }} >Presse</Link></li>
+          <li><Link to='/src/Header/Losungsansatze.js' style={{ textDecoration: 'none', color: '#5B5454', fontSize: '20px' }} >Losungsansatze</Link></li>
+          <li><Link to='/src/Header/Literatur.js'style={{ textDecoration: 'none', color: '#5B5454', fontSize: '20px' }} >Literatur</Link></li>
+          <li><Link to='/src/Header/Gewalt.js' style={{ textDecoration: 'none', color: '#5B5454', fontSize: '20px' }}>Gewalt</Link></li>
+          <li><Link to='/src/Header/Forderungen.js'style={{ textDecoration: 'none', color: '#5B5454', fontSize: '20px' }}>Forderungen</Link></li>
+          <li><Link to='/src/Header/HilfePapa.js'style={{ textDecoration: 'none', color: '#5B5454', fontSize: '20px' }}>HilfePapa</Link></li>
+          <li><Link to='/src/Header/ENGLISH.js'style={{ textDecoration: 'none', color: '#5B5454y', fontSize: '20px' }}>ENGLISH</Link></li>
         </ul>
       </div>
     )
