64a101200c00083d49b6a6d63bdc59873385df95
Mina Tahaei <bluxixi@outlook.com>
2019-07-23 09:01:31 +0430

    fix tslint errors

--- papa-help/src/Header/Header.js
+++ papa-help/src/Header/Header.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
-import {Link } from 'react-router-dom'
-export default class Header extends Component{
+import { Link } from 'react-router-dom'
+export default class Header extends Component {
   render () {
     return (
       <div>
         <ul>
-          <li><Link to ="/">Home</Link></li>
-          <li><Link to ="/src/Header/Problem.js">Problem</Link></li>
-          <li><Link to ="/src/Header/Presse.js">Presse</Link></li>
-          <li><Link to ="/src/Header/Losungsansatze.js">Losungsansatze</Link></li>
-          <li><Link to ="/src/Header/Literatur.js">Literatur</Link></li>
-          <li><Link to ="/src/Header/Gewalt.js">Gewalt</Link></li>
-          <li><Link to ="/src/Header/Forderungen.js">Forderungen</Link></li>
-          <li><Link to ="/src/Header/HilfePapa.js">HilfePapa</Link></li>
-          <li><Link to ="/src/Header/ENGLISH.js">ENGLISH</Link></li>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/src/Header/Problem.js'>Problem</Link></li>
+          <li><Link to='/src/Header/Presse.js'>Presse</Link></li>
+          <li><Link to='/src/Header/Losungsansatze.js'>Losungsansatze</Link></li>
+          <li><Link to='/src/Header/Literatur.js'>Literatur</Link></li>
+          <li><Link to='/src/Header/Gewalt.js'>Gewalt</Link></li>
+          <li><Link to='/src/Header/Forderungen.js'>Forderungen</Link></li>
+          <li><Link to='/src/Header/HilfePapa.js'>HilfePapa</Link></li>
+          <li><Link to='/src/Header/ENGLISH.js'>ENGLISH</Link></li>
         </ul>
       </div>
     )
