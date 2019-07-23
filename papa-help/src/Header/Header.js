e1bdf1efaa504367f5000806a654ff705dc699e7
Mina Tahaei <bluxixi@outlook.com>
2019-07-23 10:19:36 +0430

    add logo and horizontal menu basics

--- papa-help/src/Header/Header.js
+++ papa-help/src/Header/Header.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import pic from './papa.png'
 export default class Header extends Component {
   render () {
     return (
       <div>
-        <ul>
+        <img src={pic} className='pic' />
+        <ul className='menu-ul'>
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/src/Header/Problem.js'>Problem</Link></li>
           <li><Link to='/src/Header/Presse.js'>Presse</Link></li>
--- papa-help/src/Header/UberMich.js
+++ papa-help/src/Header/UberMich.js
@@ -4,9 +4,7 @@ export default class UberMich extends Component {
   render () {
     return (
       <div className='Header'>
-        <h1>
-        sdvdfbvg
-        </h1>
+
       </div>
     )
   }
